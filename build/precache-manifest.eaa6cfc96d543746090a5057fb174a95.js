0d1a74307c8ec7dc3f7eeac2978712e4de2e0914
shanoysinc <shanoy96@gmail.com>
2020-05-08 08:43:52 -0500

    first commit

---
+++ build/precache-manifest.eaa6cfc96d543746090a5057fb174a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a6841a6e89d1d190cf7b3c5bb18e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9834c44382fcf759b8ab",
+    "url": "/static/css/main.de5244be.chunk.css"
+  },
+  {
+    "revision": "8989cb0348f8934d5ebd",
+    "url": "/static/js/2.98356b43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.98356b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9834c44382fcf759b8ab",
+    "url": "/static/js/main.5166eca6.chunk.js"
+  },
+  {
+    "revision": "4e20bb128715b93a02aa",
+    "url": "/static/js/runtime-main.5444a424.js"
+  }
+]);
